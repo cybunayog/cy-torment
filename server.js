0f6ee219dfe8c0bca592f0fda7471cd2f7680ea7
Cy <cybunayog@gmail.com>
2019-11-19 21:16:30 -0800

    modified server.js

--- server.js
+++ server.js
@@ -6,5 +6,14 @@ var app = express();
 
 app.set("port", process.env.PORT || 6969);
 
-app.use('/schools', express.static()); //find a way to add the database here
-app.use('/students', express.static());
+app.use('/html', express.static('./public/html')); //find a way to add the database here
+app.use('/css', express.static('./public/css'));
+app.use('/js', express.static('./public/js'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(routes);
+
+app.listen(app.get("port"), function() {
+  console.log("Server started on port " + app.get("port"));
+})
