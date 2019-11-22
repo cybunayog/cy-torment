5c139cf07da072f52e1bd9adf18e2067e840866b
Cy <cybunayog@gmail.com>
2019-11-21 19:23:09 -0800

    Implemented sql connection in routes.js

--- routes.js
+++ routes.js
@@ -32,3 +32,31 @@ router.get('*', function(req, res) {
   res.status(404);
   //create an error page if you feel like it
 });
+
+//implement an ssh to the MySQL server that Seth has created
+var mysql = require('mysql2');
+var connection = mysql.createConnection({
+  host: '165.227.20.253',
+  user: 'cy',
+  password: 'bunayog',
+  database: 'Cy'
+});
+
+connection.connect(function(err) {
+  if(err) {
+    console.err('Error connecting: ' + err.stack);
+    process.exit(1);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SHOW DATABASES', function(error, results, fields) {
+  if (error) {
+    throw error;
+  } else {
+    console.log(results);
+  }
+});
+
+connection.end();
